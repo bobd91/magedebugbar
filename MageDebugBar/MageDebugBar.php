eebc618e6d9169ba68ba3c6f8ba99e95e33a77a1
Bob Davison <bob.davison@hotmail.com>
2015-01-24 17:49:00 +0000

    First steps in integrating MDB into Magento

---
+++ MageDebugBar/MageDebugBar.php
@@ -0,0 +1,14 @@
+<?php
+namespace MageDebugBar;
+
+class MageDebugBar extends DebugBar\DebugBar {
+
+    public function __construct()
+    {
+//        $this->addCollector(new ConfigCollector());
+//        $this->addCollector(new LayoutCollector());
+//        $this->addCollector(new EventCollector());
+//        $this->addCollector(new ModelCollector());
+//        $this->addCollector(new RequestCollector());
+    }
+}
